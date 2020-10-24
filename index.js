6bf3e06b786110cf302e7c227a81507f83c5f960
Shakeel <shakeelsamsudeen15@gmail.com>
2020-10-24 01:20:58 -0500

    first working tests

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path');
-// app.set('port', process.env.PORT || 3000);
-// app.set('view engine', 'html');
-// app.engine('html', require('ejs').renderFile);
 
-var map;
-function initialize() {
-    var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+app.use("/public", express.static('public'))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
---
+++ public/map.js
@@ -0,0 +1,9 @@
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 30.2672, lng: -97.7431 },
+        zoom: 12,
+    });
+    console.log('test');
+}
