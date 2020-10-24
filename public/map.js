88df788a088293659b63fd1edad876db4a7b86b2
Shakeel <shakeelsamsudeen15@gmail.com>
2020-10-24 02:08:02 -0500

    added some style elements

--- public/map.js
+++ public/map.js
@@ -1,9 +1,31 @@
 let map;
 
+function main() {
+    initMap();
+    drawLine();
+}
+
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 30.2672, lng: -97.7431 },
         zoom: 12,
     });
     console.log('test');
+}
+
+function drawLine() {
+    const flightPlanCoordinates = [
+        { lat: 37.772, lng: -122.214 },
+        { lat: 21.291, lng: -157.821 },
+        { lat: -18.142, lng: 178.431 },
+        { lat: -27.467, lng: 153.027 },
+      ];
+      const flightPath = new google.maps.Polyline({
+        path: flightPlanCoordinates,
+        geodesic: true,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+      });
+      flightPath.setMap(map);
 }
